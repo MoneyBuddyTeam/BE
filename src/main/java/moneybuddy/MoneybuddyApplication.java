d443015fb600090b7fdb6a0db0c58407862c39f6
FaithCoderLab <seinplan97@gmail.com>
2025-05-28 21:14:09 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/moneybuddy/MoneybuddyApplication.java
@@ -0,0 +1,13 @@
+package moneybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneybuddyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneybuddyBackendApplication.class, args);
+	}
+
+}
